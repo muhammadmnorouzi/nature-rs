1eb3881b3e85d2e901ea2101e7cd799ff32342a9
Muhammad Mahdi Norouzi <muhammadmnorouzi@gmail.com>
2025-04-18 14:54:07 +0300

    minor code cleanup

--- src/nature_errors.rs
+++ src/nature_errors.rs
@@ -1,13 +1,11 @@
 use std::fmt::Debug;
 
-
 pub enum NErrors {
     IndexOutOfRange,
     DivisionByZero,
 }
 
-impl PartialEq for NErrors
-{
+impl PartialEq for NErrors {
     fn eq(&self, other: &Self) -> bool {
         core::mem::discriminant(self) == core::mem::discriminant(other)
     }
